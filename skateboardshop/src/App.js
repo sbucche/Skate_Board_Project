8ad6fe8a2370312524ad6d2b88ba29983715f05b
sbucche <shriteshbucche98@gmail.com>
2023-09-14 15:50:49 +0530

    This is 14th Sept commit

--- skateboardshop/src/App.js
+++ skateboardshop/src/App.js
@@ -1,11 +1,47 @@
 import AllRoutes from "./AllRoutes/AllRoutes";
 import Navbar from "./components/Navbar";
+import "./App.css";
+const middleLinks = [
+  {
+    tab: "SURFSCATES",
+    path: "",
+  },
+  {
+    tab: "EXTRAS",
+    path: "login",
+  },
+  {
+    tab: "CLOTHES AND ACCESSORIES",
+    path: "products",
+  },
+  {
+    tab: "SALE",
+    path: "xyz",
+  },
+]; 
+
+const endLinks = [
+  {
+    tab: "SEARCH",
+    path: "sdf",
+  },
+  {
+    tab: "WISHLIST",
+    path: "dfg",
+  },
+  {
+    tab: "BAG",
+    path: "cart",
+  }
+];
+
+
 
 function App() {
   return (
-    <div className="App">
-        <Navbar/>
-        <AllRoutes/>
+    <div className="App">    
+        <Navbar middleLinks={middleLinks} endLinks={endLinks}/>
+        <AllRoutes/>    
     </div>
   );
 }
--- skateboardshop/src/pages/Products.jsx
+++ skateboardshop/src/pages/Products.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    
+    <div>
+        <h1>This products page</h1>
+    </div>
+
   )
 }
 
