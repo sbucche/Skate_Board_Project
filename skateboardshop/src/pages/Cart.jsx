907499016f32d2457d5ca306172461810a534717
AnandAK07 <anandkumark055@gmail.com>
2023-09-16 22:38:24 +0530

    cartPage

--- skateboardshop/src/pages/Cart.jsx
+++ skateboardshop/src/pages/Cart.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCartAction } from '../redux/CartReducer/action'
+
 
 const Cart = () => {
+  const {isLoading,isError,cartData}=useSelector((store)=>store.reducer)
+  const dispatch=useDispatch()
+  console.log("id",cartData)
+
+  useEffect(()=>{
+    dispatch(getCartAction())
+    console.log("1")
+  },[dispatch])
+
+
   return (
-    <div>Cart</div>
+    <div>
+      <h1>Cart</h1>
+      {cartData?.map((el)=>(
+          <div key={el.id}>
+            <img src={el.image} alt="" />
+            <h3>{el.title}</h3>
+            <p>{el.price}</p>
+          </div>
+      ))}
+    </div>
   )
 }
 
