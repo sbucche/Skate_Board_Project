9ebc096f45aa8a7e40ef00d9bbc16a96e96e94e3
AnandAK07 <anandkumark055@gmail.com>
2023-09-17 01:22:24 +0530

    CRUD

--- skateboardshop/src/pages/Cart.jsx
+++ skateboardshop/src/pages/Cart.jsx
@@ -1,29 +1,44 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getCartAction } from '../redux/CartReducer/action'
+import Row from '../components/Row'
 
 
 const Cart = () => {
   const {isLoading,isError,cartData}=useSelector((store)=>store.reducer)
   const dispatch=useDispatch()
-  console.log("id",cartData)
+  // console.log("id",cartData)
 
+  const cartProducts=cartData.filter(el=>el.quantity>=1);
+  
+  // console.log("filter",cartProducts)
   useEffect(()=>{
     dispatch(getCartAction())
-    console.log("1")
+    // console.log("1")
   },[dispatch])
 
-
+  
   return (
     <div>
       <h1>Cart</h1>
-      {cartData?.map((el)=>(
+      {/* {cartData?.map((el)=>(
           <div key={el.id}>
             <img src={el.image} alt="" />
             <h3>{el.title}</h3>
             <p>{el.price}</p>
+            <div>
+              <button>+</button>
+              <button>-</button>
+            </div>
+            <button>Remove</button>
           </div>
-      ))}
+      ))} */}
+      
+      {
+        cartProducts?.map((el)=>(
+          <Row data={el} key={el.id}/>
+        ))
+      }
     </div>
   )
 }
